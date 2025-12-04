d821cec7f1e8fa1a6e0bc265776fd7b934542dbd
Jakub <45879657+zeedivx@users.noreply.github.com>
2025-12-04 14:34:23 +0100

    feat: add utility functions for standardized JSON responses

---
+++ internal/utils/response.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// SuccessResponse sends a success JSON response
+func SuccessResponse(c *fiber.Ctx, data any, message string, code ...int) error {
+	statusCode := fiber.StatusOK
+	if len(code) > 0 {
+		statusCode = code[0]
+	}
+
+	return c.Status(statusCode).JSON(fiber.Map{
+		"success": true,
+		"data":    data,
+		"message": message,
+	})
+}
+
+// ErrorResponse sends an error JSON response
+func ErrorResponse(c *fiber.Ctx, message string, code ...int) error {
+	statusCode := fiber.StatusInternalServerError
+	if len(code) > 0 {
+		statusCode = code[0]
+	}
+
+	return c.Status(statusCode).JSON(fiber.Map{
+		"success": false,
+		"error":   message,
+	})
+}
