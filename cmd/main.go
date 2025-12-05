ccda911103ddaa736549cc39a3bd12f21c425647
Jakub <45879657+zeedivx@users.noreply.github.com>
2025-12-05 09:39:23 +0100

    feat: refactor main application to use a dedicated server package for initialization and routing

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,7 @@ import (
 	"os"
 
 	"github.com/Anvoria/authly/internal/config"
-	"github.com/Anvoria/authly/internal/database"
-	"github.com/Anvoria/authly/internal/router"
-	"github.com/gofiber/fiber/v2"
+	"github.com/Anvoria/authly/internal/server"
 )
 
 func main() {
@@ -19,45 +17,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	initLogger(cfg.Logging.Level)
-
-	app := fiber.New()
-
-	if err := database.ConnectDB(cfg); err != nil {
-		slog.Error("Failed to connect to database", "error", err)
-		os.Exit(1)
-	}
-	slog.Info("Database connected successfully")
-
-	router.SetupRoutes(app)
-
-	addr := cfg.Server.Address()
-	slog.Info("Server starting", "address", addr)
-	if err := app.Listen(addr); err != nil {
+	if err := server.Start(cfg); err != nil {
 		slog.Error("Failed to start server", "error", err)
 		os.Exit(1)
 	}
-}
-
-func initLogger(level string) {
-	var logLevel slog.Level
-	switch level {
-	case "debug":
-		logLevel = slog.LevelDebug
-	case "info":
-		logLevel = slog.LevelInfo
-	case "warn":
-		logLevel = slog.LevelWarn
-	case "error":
-		logLevel = slog.LevelError
-	default:
-		logLevel = slog.LevelInfo
-	}
-
-	opts := &slog.HandlerOptions{
-		Level: logLevel,
-	}
-
-	handler := slog.NewTextHandler(os.Stdout, opts)
-	slog.SetDefault(slog.New(handler))
 }
---
+++ internal/server/http.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Anvoria/authly/internal/config"
+	"github.com/Anvoria/authly/internal/database"
+	"github.com/Anvoria/authly/internal/router"
+	"github.com/gofiber/fiber/v2"
+)
+
+// Start initializes and starts the HTTP server
+func Start(cfg *config.Config) error {
+	initLogger(cfg.Logging.Level)
+
+	app := fiber.New()
+
+	if err := database.ConnectDB(cfg); err != nil {
+		slog.Error("Failed to connect to database", "error", err)
+		return err
+	}
+	slog.Info("Database connected successfully")
+
+	router.SetupRoutes(app)
+
+	addr := cfg.Server.Address()
+	slog.Info("Server starting", "address", addr)
+	if err := app.Listen(addr); err != nil {
+		slog.Error("Failed to start server", "error", err)
+		return err
+	}
+
+	return nil
+}
+
+func initLogger(level string) {
+	var logLevel slog.Level
+	switch level {
+	case "debug":
+		logLevel = slog.LevelDebug
+	case "info":
+		logLevel = slog.LevelInfo
+	case "warn":
+		logLevel = slog.LevelWarn
+	case "error":
+		logLevel = slog.LevelError
+	default:
+		logLevel = slog.LevelInfo
+	}
+
+	opts := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+
+	handler := slog.NewTextHandler(os.Stdout, opts)
+	slog.SetDefault(slog.New(handler))
+}
